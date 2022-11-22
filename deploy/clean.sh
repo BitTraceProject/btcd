815390740b23c1e3e3afe257cad0b47010580edf
ZJH <zjh98@vip.qq.com>
2022-11-22 17:22:31 +0800

    test 1122-14

--- deploy/clean.sh
+++ deploy/clean.sh
@@ -5,6 +5,7 @@ temp_dir=temp
 pwd=${PWD}
 
 function clean() {
+  source ${pwd}/peers/${CONTAINER_NAME}/.env
   docker stop ${CONTAINER_NAME}
   echo "stop ${CONTAINER_NAME}"
   docker rm ${CONTAINER_NAME}

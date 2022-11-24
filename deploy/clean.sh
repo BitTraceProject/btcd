002442622f8a120275b8faa5b0e3191a34c94da7
ZJH <zjh98@vip.qq.com>
2022-11-24 18:00:56 +0800

    test 1124-16

--- deploy/clean.sh
+++ deploy/clean.sh
@@ -3,16 +3,11 @@
 pwd=${HOME}/.bittrace
 peer_pwd=${pwd}/peers
 
-function clean() {
-  CONTAINER_NAME=$1
-  source ${peer_pwd}/${CONTAINER_NAME}/.env
-  docker stop ${CONTAINER_NAME}
-  echo "stop ${CONTAINER_NAME}"
-  docker rm ${CONTAINER_NAME}
-  echo "rm ${CONTAINER_NAME}"
-  sudo rm -rf ${peer_pwd}/${CONTAINER_NAME}
-  echo "rm -rf ${peer_pwd}/${CONTAINER_NAME}"
-
-}}
-
-clean $1
+CONTAINER_NAME=$1
+source ${peer_pwd}/${CONTAINER_NAME}/.env
+docker stop ${CONTAINER_NAME}
+echo "stop ${CONTAINER_NAME}"
+docker rm ${CONTAINER_NAME}
+echo "rm ${CONTAINER_NAME}"
+sudo rm -rf ${peer_pwd}/${CONTAINER_NAME}
+echo "rm -rf ${peer_pwd}/${CONTAINER_NAME}"
